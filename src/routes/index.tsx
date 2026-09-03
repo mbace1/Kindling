a68fb7e0d83d3bebaf897700e6a2a383e9bafd35
mbace1 <mikael.haveri@gmail.com>
2026-09-03 15:06:39 -0700

    Mount reusable game feel layer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
+import { GameFeelLayer } from "@/components/game-feel-layer";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <AppShell />;
+  return (
+    <>
+      <AppShell />
+      <GameFeelLayer />
+    </>
+  );
 }
